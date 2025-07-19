a493ea92cce8e3034f84246c0d6434031bb4f787
ADMIN <harishkcs00@gmail.com>
2025-07-19 06:23:30 -0700

    API with ADO.NET for fetch

--- Bayer-API/Controllers/BMIController.cs
+++ Bayer-API/Controllers/BMIController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service;
+using Service.DTOs;
 using Service.Interfaces;
 
 namespace Bayer_API.Controllers
@@ -9,6 +12,8 @@ namespace Bayer_API.Controllers
     public class BMIController : ControllerBase
     {
         readonly IBMIService _bMIService;
+        private readonly string connectionString = "Server=localhost;Database=bayer-health-care-dev;Trusted_Connection=True;";
+
         public BMIController(IBMIService bMIService)
         {
             _bMIService = bMIService;
@@ -17,8 +22,45 @@ namespace Bayer_API.Controllers
         [HttpGet("username")]
         public IActionResult GetBMIbyUsername(string username)
         {
-            var result = _bMIService.GetBMIbyUserName(username);
-            return Ok(result);
+            UserDTO userDTO = new UserDTO();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Example query
+                    string query = "SELECT UserID,UserName,Age,Gender,Height,Weight FROM tblUser where UserName=@username";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@username", username);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            userDTO = new UserDTO();
+                            userDTO.UserID = Convert.ToInt32(reader["UserID"]);
+                            userDTO.UserName = Convert.ToString(reader["UserName"]);
+                            userDTO.Age = Convert.ToInt32(reader["Age"]);
+                            userDTO.Height = Convert.ToDecimal(reader["Height"]);
+                            userDTO.Weight = Convert.ToInt32(reader["Weight"]);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"❌ Error: {ex.Message}");
+                }
+            }
+            var bMIDto = new BMIDto();
+            bMIDto.UserID = userDTO.UserID;
+            bMIDto.Name = userDTO.UserName;
+            if (userDTO.Weight.HasValue && userDTO.Weight.Value > 0)
+            {
+                decimal weight = userDTO.Weight.Value;
+                bMIDto.BMI = (decimal)(weight / (userDTO.Height * userDTO.Height));
+            }
+            return Ok(bMIDto);
         }
     }
 }
--- Bayer-API/Controllers/UserController.cs
+++ Bayer-API/Controllers/UserController.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Service.DTOs;
 using Service.Interfaces;
 
@@ -10,23 +12,91 @@ namespace Bayer_API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        readonly IUserService _userService;
-        public UserController(IUserService userService) {         
-            _userService = userService;
+        //readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
+        private readonly string connectionString = "Server=localhost;Database=bayer-health-care-dev;Trusted_Connection=True;";
+
+        public UserController(IConfiguration configuration,IUserService userService) {         
+           // _userService = userService;
+            _configuration = configuration;
         }
 
         [HttpGet("username")]
-        public async Task<IActionResult> GetUserByUserName([FromQuery] string username)
+        public IActionResult GetUserByUserName([FromQuery] string username)
         {
-          var result= await _userService.GetUser(username);
-           return Ok(result);
+            UserDTO userDTO = new UserDTO();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Example query
+                    string query = "SELECT UserID,UserName,Age,Gender,Height,Weight FROM tblUser where UserName=@username";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@username", username);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            userDTO = new UserDTO();
+                            userDTO.UserID = Convert.ToInt32(reader["UserID"]);
+                            userDTO.UserName = Convert.ToString(reader["UserName"]);
+                            userDTO.Age = Convert.ToInt32(reader["Age"]);
+                            userDTO.Height = Convert.ToDecimal(reader["Height"]);
+                            userDTO.Weight = Convert.ToInt32(reader["Weight"]);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"❌ Error: {ex.Message}");
+                }
+            }
+           // var result= await _userService.GetUser(username);
+           return Ok(userDTO);
         }
 
         [HttpPost]
         public async Task<IActionResult> InsertUser([FromBody] UserDTO userDTO)
         {
-            var result = await _userService.InsertUser(userDTO);
+            var result = "";// await _userService.InsertUser(userDTO);
             return Ok(result);
         }
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new List<string>();
+            string connectionString = "Server=localhost;Database=bayer-health-care-dev;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("✅ Connection established successfully.");
+
+                    // Example query
+                    string query = "SELECT UserID,UserName,Age,Gender,Height,Weight FROM tblUser";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["Username"]);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"❌ Error: {ex.Message}");
+                }
+            }
+
+            return Ok(users);
+        }
+
     }
 }
